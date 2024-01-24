949d7bf35cdc37bde81f445d9223e965a6d00ee8
Martin Beran <martin@mber.cz>
2024-01-24 09:37:59 +0100

    Alternative implementation of start/stop buttons

--- led_blink_vhdl/blink.vhd
+++ led_blink_vhdl/blink.vhd
@@ -2,7 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- two alternatively blinking LEDs
+-- two pairs of alternatively blinking LEDs
+-- uses two implementations of blinking on/off switch
 
 entity blink is
 	generic (
@@ -11,35 +12,58 @@ entity blink is
 	);
 	port (
 		clk, start, stop: in std_logic;
-		led1, led2: out std_logic := '1'
+		led1, led2, led3, led4: out std_logic := '1'
 	);
 end entity;
 
 architecture main of blink is
+	signal running12: std_logic := '1';
 begin
-	process (clk) is
+	running12 <=
+		'0' when stop = '0' else
+		'1' when start = '0' else
+		unaffected;
+	led12: process is
 		variable cnt: integer := 0;
 		variable state: std_logic := '0';
-		variable running: boolean := true;
 	begin
-		if rising_edge(clk) then
-			if stop = '0' then
-				running := false;
-			elsif start = '0' then
-				running := true;
-			end if;
-			cnt := cnt + 1;
-			if cnt = crystal_khz * period_ms then
-				cnt := 0;
-				state := not state;
-			end if;
+		cnt := cnt + 1;
+		if cnt = crystal_khz * period_ms then
+			cnt := 0;
+			state := not state;
 		end if;
-		if running then
+		if running12 = '1' then
 			led1 <= state;
 			led2 <= not state;
 		else
 			led1 <= '1';
 			led2 <= '1';
+		end if;
+		wait on clk until clk = '1';
+	end process;
+	led34: process (clk) is
+		variable cnt: integer := 0;
+		variable state: std_logic := '0';
+		variable running34: boolean := true;
+	begin
+		if rising_edge(clk) then
+			if stop = '0' then
+				running34 := false;
+			elsif start = '0' then
+				running34 := true;
+			end if;
+			cnt := cnt + 1;
+			if cnt = 2 * crystal_khz * period_ms then
+				cnt := 0;
+				state := not state;
+			end if;
+		end if;
+		if running34 then
+			led3 <= state;
+			led4 <= not state;
+		else
+			led3 <= '1';
+			led4 <= '1';
 		end if;
 	end process;
 end architecture;
--- led_blink_vhdl/tb_blink.vhd
+++ led_blink_vhdl/tb_blink.vhd
@@ -7,7 +7,7 @@ end entity;
 architecture main of tb_blink is
 	signal tClk: std_logic := '0';
 	signal tStart, tStop: std_logic := '1';
-	signal led1, led2: std_logic;
+	signal led1, led2, led3, led4: std_logic;
 begin
 	process
 	begin
@@ -15,9 +15,12 @@ begin
 		tClk <= not tClk;
 	end process;
 	tStop <=
-		'0' after 200 ns,
-		'1' after 400 ns;
+		'0' after 202 ns,
+		'1' after 217 ns;
+	tStart <=
+		'0' after 402 ns,
+		'1' after 416 ns;
 	uub: entity work.blink
 		generic map (crystal_khz=>1, period_ms=>10)
-		port map (clk=>tClk, start=>tStart, stop=>tStop, led1=>led1, led2=>led2);
+		port map (clk=>tClk, start=>tStart, stop=>tStop, led1=>led1, led2=>led2, led3=>led3, led4=>led4);
 end;
