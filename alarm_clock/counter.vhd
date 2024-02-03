70f200755ec582c00e8559a3d5c59b3a634e3230
Martin Beran <martin@mber.cz>
2024-02-04 00:58:06 +0100

    WiP: entity counter

---
+++ alarm_clock/counter.vhd
@@ -0,0 +1,18 @@
+-- Time keeping
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity counter is
+	generic (
+		-- TODO
+	);
+	port (
+		-- TODO
+	);
+end entity;
+
+architecture main of counter is
+begin
+	-- TODO
+end architecture;
