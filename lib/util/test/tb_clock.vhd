b4cd34d69a22ceabff1da1ef60ddbcc11d6dfe1e
Martin Beran <martin@mber.cz>
2024-02-16 00:35:50 +0100

    Added another clock divider to testbench

--- lib/util/test/tb_clock.vhd
+++ lib/util/test/tb_clock.vhd
@@ -9,7 +9,7 @@ entity tb_clock_divider is
 end entity;
 
 architecture main of tb_clock_divider is
-	signal Clk, Rst, O1, O2, O3, O2_1, O2_2, O2_3, O3_2, O2_3_2: std_logic;
+	signal Clk, Rst, O1, O2, O3, O2_1, O2_2, O2_3, O3_2, O2_3_2, O2_3_2_3: std_logic;
 begin
 	clock: process is
 		variable state: std_logic := '0';
@@ -29,4 +29,5 @@ begin
 	dut2_3: clock_divider generic map (use_I=>true, factor=>3) port map (Clk=>Clk, Rst=>Rst, I=>O2, O=>O2_3);
 	dut3_2: clock_divider generic map (use_I=>true, factor=>2) port map (Clk=>Clk, Rst=>Rst, I=>O3, O=>O3_2);
 	dut2_3_2: clock_divider generic map (use_I=>true, factor=>2) port map (Clk=>Clk, Rst=>Rst, I=>O2_3, O=>O2_3_2);
+	dut2_3_2_3: clock_divider generic map (use_I=>true, factor=>3) port map (Clk=>Clk, Rst=>Rst, I=>O2_3_2, O=>O2_3_2_3);
 end architecture;
