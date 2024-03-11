b3bf5fea670257e4fb7fb7a72fbc4f25bdef7a82
Martin Beran <martin@mber.cz>
2024-03-11 23:54:14 +0100

    generated PLL for VGA pixel clock
    in Quartus 22.1, because the PLL wizard crashes in 23.1

--- lib/io/vga.vhd
+++ lib/io/vga.vhd
@@ -1,4 +1,23 @@
 -- VGA controller
+-- It implements mode 640x480 @ 60 Hz
+-- General timing
+--     Screen refresh rate      60 Hz
+--     Vertical refresh         31.46875 kHz
+--     Pixel freq.              25.175 MHz
+-- Horizontal timing
+--     Scanline part    Pixels  Time [µs]
+--     Visible area     640     25.422045680238
+--     Front porch      16      0.63555114200596
+--     Sync pulse       96      3.8133068520357
+--     Back porch       48      1.9066534260179
+--     Whole line       800     31.777557100298
+-- Vertical timing
+--     Frame part       Lines   Time [ms]
+--     Visible area     480     15.253227408143
+--     Front porch      10      0.31777557100298
+--     Sync pulse       2       0.063555114200596
+--     Back porch       33      1.0486593843098
+--     Whole frame      525     16.683217477656
 
 library ieee;
 use ieee.std_logic_1164;
