378f63c06157c3e828c0e06200d0b04426931f6e
Martin Beran <martin@mber.cz>
2024-10-20 23:13:34 +0200

    WiP: serial communication protocol of CDI

--- mb50/mb50/cdi.vhd
+++ mb50/mb50/cdi.vhd
@@ -45,6 +45,20 @@ entity cdi is
 		-- Direct write to memory (valid address on AddrBus, valid data on DataBusWr)
 		Wr: out std_logic
 	);
+	subtype UartData is std_logic_vector(7 downto 0); -- Data type of the serial port
+	-- CDI reguests (received from the debugger) ------------------------------
+	-- Request for a status, expects RespStatus
+	constant ReqStatus: UartData := X"00";
+	-- Request for executing a single instruction, expects RespStatus
+	constant ReqStep: UartData := X"01";
+	-- CDI responses (sent to the debugger) -----------------------------------
+	-- Returned for an unknown request code
+	constant RespUnknownReq: UartData := X"00";
+	-- System status, followed by bytes:
+	-- 1. '0000000H'; H = CPU signal Halted
+	-- 2. lower byte of register PC
+	-- 3. upper byte of register PC
+	constant RespStatus: UartData := X"01";
 end entity;
 
 architecture main of cdi is
@@ -60,23 +74,98 @@ begin
 		TxD=>uart_txd, TxStart=>uart_tx_start, TxReady=>uart_tx_ready, TxBreak=>uart_tx_break,
 		RxD=>uart_rxd, RxValid=>uart_rx_valid, RxAck=>uart_rx_ack, RxErr=>uart_rx_err, RxBreak=>uart_rx_break
 	);
-	-- TODO
-	uart_cfg_set <= '0';
-	uart_cfg_frame <= '0';
-	uart_cfg <= (others=>'0');
-	uart_txd <= (others=>'0');
-	uart_tx_start <= '0';
-	uart_tx_break <= '0';
-	uart_rx_ack <= '0';
 	
-	CpuRun <= '1';
-	CpuRegIdx <= (others=>'0');
-	CpuRegDataWr <= (others=>'0');
-	CpuRegRd <= '0';
-	CpuRegWr <= '0';
-	CpuRegCsr <= '0';
-	AddrBus <= (others=>'0');
-	DataBusWr <= (others=>'0');
-	Rd <= '0';
-	Wr <= '0';
+	cdi_fsm: block is
+		type state_t is (
+			Init, -- Initial state
+			WaitNotRun, -- Wait for CPU not running
+			Ready, -- Serial interface configured and listening for a command from the debugger
+			SendUnknownReq, -- Send RespUnknownReq
+			SendStatus, -- Send RespStatus
+			SendStatus1,
+			SendStatus2,
+			SendStatus3,
+			DoStep -- Execute a single instruction
+		);
+		signal state: state_t := Init;
+	begin
+		run: process (Rst, Clk) is
+			procedure init_signals is
+			begin
+				CpuRun <= '0';
+				CpuRegIdx <= (others=>'0');
+				CpuRegDataWr <= (others=>'0');
+				CpuRegRd <= '0';
+				CpuRegWr <= '0';
+				CpuRegCsr <= '0';
+				AddrBus <= (others=>'0');
+				DataBusWr <= (others=>'0');
+				Rd <= '0';
+				Wr <= '0';
+				uart_cfg_set <= '0';
+				uart_cfg_frame <= '0';
+				uart_cfg <= (others=>'0');
+				uart_txd <= (others=>'0');
+				uart_tx_start <= '0';
+				uart_tx_break <= '0';
+				uart_rx_ack <= '0';
+			end procedure;
+			procedure send_byte(byte: UartData; new_state: state_t := WaitNotRun) is
+			begin
+				if uart_tx_ready = '1' then
+					uart_txd <= byte;
+					uart_tx_start <= '1';
+					state <= new_state;
+				end if;
+			end procedure;
+		begin
+			if Rst = '1' then
+				init_signals;
+				state <= Init;
+			elsif rising_edge(Clk) then
+				init_signals;
+				case state is
+					when Init =>
+						-- Set serial port speed
+						uart_cfg_set <= '1';
+						uart_cfg <= pkg_uart.uart_baud_115200;
+						state <= WaitNotRun;
+					when WaitNotRun =>
+						if CpuBusy /= '1' then
+							state <= Ready;
+						end if;
+					when Ready =>
+						if uart_rx_valid = '1' then
+							uart_rx_ack <= '1';
+							case uart_rxd is
+								when ReqStatus =>
+									state <= SendStatus;
+								when ReqStep =>
+									-- Allow 1 cycle for CPU to signal Busy='1'
+									CpuRun <= '1';
+									state <= DoStep;
+								when others =>
+									state <= SendUnknownReq;
+							end case;
+						end if;
+					when SendUnknownReq =>
+						send_byte(RespUnknownReq, WaitNotRun);
+					when SendStatus =>
+						send_byte(RespStatus, SendStatus1);
+					when SendStatus1 =>
+						CpuRegIdx <= to_reg_idx(reg_idx_pc);
+						CpuRegRd <= '1';
+						send_byte("0000000" & CpuHalted, SendStatus2);
+					when SendStatus2 =>
+						CpuRegIdx <= to_reg_idx(reg_idx_pc);
+						CpuRegRd <= '1';
+						send_byte(std_logic_vector(CpuRegDataRd(7 downto 0)), SendStatus3);
+					when SendStatus3 =>
+						send_byte(std_logic_vector(CpuRegDataRd(15 downto 8)));
+					when DoStep =>
+						state <= WaitNotRun;
+				end case;
+			end if;
+		end process;
+	end block;
 end architecture;
